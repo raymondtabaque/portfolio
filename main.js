d98753f4569128a422911faaef614a717ab326bf
Raymond Tabaque <raymondtabaque@gmail.com>
2023-05-31 14:17:12 +0700

    togbutton

--- main.js
+++ main.js
@@ -1,45 +1,37 @@
 let activateButton = document.getElementById('headerbutton');
-activateButton.onclick = nightMode;
+activateButton.onclick = togButton;
+
+function togButton () {
+    if (document.getElementsByTagName('p')[0].innerHTML === 'Night mode') {
+        nightMode();
+    } else {
+        lightMode();
+    }
+}
 
 
 function nightMode () {
     let bgcolor = document.body.style;
     bgcolor.color = 'white';
     bgcolor.backgroundColor='black';
-    document.getElementById('headerbutton').style.backgroundColor ='purple';
+    document.getElementById('headerbutton').style.backgroundColor ='gray';
     document.getElementsByTagName('p')[0].innerHTML ='Light mode';
     document.getElementsByTagName('header')[0].style.backgroundColor='black';
 
     for (let i = 0; i < document.getElementsByClassName('projects').length; i++ ) {
-        document.getElementsByClassName('projects')[i].style.backgroundColor = 'navy';
+        document.getElementsByClassName('projects')[i].style.borderColor = 'white';
     }
 }
 
+function lightMode () {
+    let bcolor = document.body.style;
+    bcolor.color = 'black';
+    bcolor.backgroundColor='white';
+    document.getElementById('headerbutton').style.backgroundColor ='#f2c300';
+    document.getElementsByTagName('p')[0].innerHTML ='Night mode';
+    document.getElementsByTagName('header')[0].style.backgroundColor='navy';
 
-
-
-
-
-/*function changeColor () {
-    let bgcolor = document.body;
-    let randomColor = '';
-    let y = Math.floor(Math.random()*5);
-    switch (y) {
-        case 0:
-            randomColor = 'beige';
-            break;
-        case 1:
-            randomColor = 'F0FFFF';
-            break;
-        case 2:
-            randomColor = '#F5F5F5';
-            break;
-        case 3:
-            randomColor = '#DCDCDC';
-            break;
-        case 4:
-            randomColor = 'white';
-            break;
-      }
-      bgcolor.style.backgroundColor = randomColor;
-    }*/
+    for (let i = 0; i < document.getElementsByClassName('projects').length; i++ ) {
+        document.getElementsByClassName('projects')[i].style.borderColor = 'black';
+    }
+}
